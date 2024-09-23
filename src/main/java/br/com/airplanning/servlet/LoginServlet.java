a18310ffee4bef4de79152ba9feee4a2ccc96317
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-09-23 18:11:48 -0300

    feat: add modelagem de banco de dados, estrutura inicial da servlet de login e dependencias

---
+++ src/main/java/br/com/airplanning/servlet/LoginServlet.java
@@ -0,0 +1,19 @@
+package br.com.airplanning.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String senha = req.getParameter("senha");
+
+
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: pedro
+  Date: 9/23/2024
+  Time: 12:53 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
