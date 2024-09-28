17cd0bfee47bf8a6ab788bced85965dc0f59674d
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-09-28 10:59:46 -0300

    feat: servlet de cadastro de user e de login com metodos doGet e doPost

--- src/main/java/br/com/airplanning/servlet/CreateUserServlet.java
+++ src/main/java/br/com/airplanning/servlet/CreateUserServlet.java
@@ -1,2 +1,64 @@
-package br.com.airplanning.servlet;public class CreateUserServlet {
+package br.com.airplanning.servlet;
+
+import br.com.airplanning.enums.TypeCustomer;
+import br.com.airplanning.model.Customer;
+import br.com.airplanning.repository.customer.CustomerRepository;
+import br.com.airplanning.security.Criptografia;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+@WebServlet("/cadastro")
+public class CreateUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("cadastro.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nome = req.getParameter("nome");
+        String telefone = req.getParameter("telefone");
+        String email = req.getParameter("email");
+        String senha = req.getParameter("senha");
+        String confirmar_senha = req.getParameter("confirmar-senha");
+        String cpf = req.getParameter("cpf");
+
+
+        String senhaCriptografada = null;
+        try {
+            senhaCriptografada = Criptografia.converterParaMD5(senha);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        boolean confirmPassword = senha.equalsIgnoreCase(confirmar_senha);
+
+        if (confirmPassword) {
+            try {
+                Customer customer = new Customer(nome, telefone, email, senhaCriptografada, cpf, TypeCustomer.CLIENTE);
+                new CustomerRepository().createCustomer(customer);
+                req.getSession().setAttribute("successMessage", "O usuário foi cadastrado com sucesso!");
+                resp.sendRedirect("/login");
+                return;
+
+
+            } catch (Exception e) {
+                req.setAttribute("fail", "Não foi possível realizar o cadastro!");
+                req.getRequestDispatcher("cadastro.jsp").forward(req, resp);
+                return;
+            }
+
+        }
+        req.setAttribute("failpassword", "As senhas não são iguais!");
+        req.getRequestDispatcher("cadastro.jsp").forward(req, resp);
+    }
+
+
 }
--- src/main/java/br/com/airplanning/servlet/LoginServlet.java
+++ src/main/java/br/com/airplanning/servlet/LoginServlet.java
@@ -1,19 +1,60 @@
 package br.com.airplanning.servlet;
 
+import br.com.airplanning.model.Customer;
+import br.com.airplanning.repository.customer.CustomerRepository;
+import br.com.airplanning.security.Criptografia;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String email = req.getParameter("email");
         String senha = req.getParameter("senha");
 
+        CustomerRepository customerRepository = new CustomerRepository();
 
+        // Chama o método findCustomerByEmail para buscar o e-mail fornecido no cadastro em nosso BD.
+        Customer customer = customerRepository.findCustomerByEmail(email);
+
+
+        if (customer != null) {
+            String senhaCriptografada = null;
+            try {
+                senhaCriptografada = Criptografia.converterParaMD5(senha);
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+
+            // Se o cliente for encontrado, ele criptografa a senha fornecida pelo usuário para comparar com a senha do BD.
+            if (customer.getPassword().equals(senhaCriptografada)) {
+
+                // Se a senha for correta, ele é redirecionado para a home.
+                req.getSession().setAttribute("user", customer);
+                req.getSession().setAttribute("successMessage", "Login realizado com sucesso!");
+                resp.sendRedirect("/home");
+            } else {
+                // Se a senha for incorreta, ele permanece no login!
+                req.getSession().setAttribute("errorMessage", "Email ou senha incorretos!");
+                resp.sendRedirect("/login");
+            }
+        } else {
+            // Usuário não encontrado
+            req.getSession().setAttribute("errorMessage", "Email ou senha incorretos!");
+            resp.sendRedirect("/login");
+        }
     }
 }
