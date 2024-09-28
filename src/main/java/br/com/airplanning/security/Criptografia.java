6f2e63d9a0c2febacff087200de711eaf9ff856c
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-09-28 10:57:18 -0300

    feat: classe de criptografia de senha para comparar a senha fornecida com a armazenada no BD

--- src/main/java/br/com/airplanning/security/Criptografia.java
+++ src/main/java/br/com/airplanning/security/Criptografia.java
@@ -1,2 +1,18 @@
-package br.com.airplanning.security;public class Criptografia {
+package br.com.airplanning.security;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Criptografia {
+
+        // Fizemos este método para converter a senha em um Hash MD5. Ele é necessário para comparar a senha fornecida com a armazenada no BD.
+        public static String converterParaMD5(String senha) throws NoSuchAlgorithmException {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(senha.getBytes());
+            byte[] digest = md.digest();
+            String resultado = new String(Hex.encodeHex(digest));
+            return resultado;
+        }
 }
