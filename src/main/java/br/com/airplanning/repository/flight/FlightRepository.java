345552c9d20c95d76e04110b2c0b5092ae20b080
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-10-11 13:02:13 -0300

    feat: create flight repository

--- src/main/java/br/com/airplanning/repository/flight/FlightRepository.java
+++ src/main/java/br/com/airplanning/repository/flight/FlightRepository.java
@@ -1,2 +1,12 @@
-package br.com.airplanning.repository.flight;public class FlightRepository {
+package br.com.airplanning.repository.flight;
+
+import br.com.airplanning.model.Flight;
+
+public class FlightRepository {
+
+    public void createFlight(Flight flight) {
+
+
+    }
+
 }
