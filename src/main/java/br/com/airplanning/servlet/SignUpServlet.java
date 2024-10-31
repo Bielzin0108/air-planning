82bb4e15ca40ea77549de1d0eaeda0b30c7c8f67
pedrozardetti <pedrozardetti1@gmail.com>
2024-10-30 21:34:18 -0300

    fix: adjust sign up servlet attributes

--- src/main/java/br/com/airplanning/servlet/SignUpServlet.java
+++ src/main/java/br/com/airplanning/servlet/SignUpServlet.java
@@ -1,22 +1,59 @@
 package br.com.airplanning.servlet;
 
+import br.com.airplanning.enums.TypeCustomer;
+import br.com.airplanning.model.Customer;
+import br.com.airplanning.repository.customer.CustomerRepository;
+import br.com.airplanning.security.Criptografia;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 @WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/signup.jsp").forward(req, resp);
+        req.getRequestDispatcher("signup.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+
+        String nome = req.getParameter("nome");
+
+        String numero = req.getParameter("numero");
+
+        String email = req.getParameter("email");
+
+        String senha = req.getParameter("senha");
+
+        String cpf = req.getParameter("cpf");
+
+        String senhaCriptografada = null;
+        try {
+            senhaCriptografada = Criptografia.converterParaMD5(senha);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Customer customer = new Customer(nome, numero, email, senhaCriptografada, cpf, TypeCustomer.CLIENTE);
+            new CustomerRepository().createCustomer(customer);
+            req.getSession().setAttribute("successMessage", "O usuário foi cadastrado com sucesso!");
+            resp.sendRedirect("/login");
+            return;
+
+
+        } catch (Exception e) {
+            req.setAttribute("fail", "Não foi possível realizar o cadastro!");
+            req.getRequestDispatcher("cadastro.jsp").forward(req, resp);
+            return;
+        }
     }
 }
