c1c691be4f2abf05849ae31676e80a08da36ccc7
pedrozardetti <pedrozardetti1@gmail.com>
2024-10-30 21:33:36 -0300

    feat: add new base redirect function

--- src/main/java/br/com/airplanning/filter/RootRedirectionFilter.java
+++ src/main/java/br/com/airplanning/filter/RootRedirectionFilter.java
@@ -1,4 +1,35 @@
 package br.com.airplanning.filter;
 
-public class RootRedirectionFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class RootRedirectionFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        String uri = httpServletRequest.getRequestURI();
+
+        if (uri.equals("/") || uri.equals("/airplanning/")) {
+            httpServletResponse.sendRedirect("/login");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
